cf271f4f7f0a64e9825f1316d49bd7fb3da1640a
I_Am_Err00r <40243263+I-Am-Err00r@users.noreply.github.com>
2020-11-24 21:41:08 -0600

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
